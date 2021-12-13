dc7142d1c95d45efa9c242440b7b4d27f92d041b
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-14 08:30:41 +0900

    axiosをインストール

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,22 @@
+import { VFC } from "react";
+import { UserProfile } from "../types/UserProfile";
+
+type Props = {
+  user: UserProfile;
+};
+
+export const UserCard: VFC<Props> = (props) => {
+  const { user } = props;
+  return(
+    <div>
+      <dl>
+        <dt>名前</dt>
+        <dd>{user.name}</dd>
+        <dt>住所</dt>
+        <dd>{user.address}</dd>
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+      </dl>
+    </div>
+  );
+}
