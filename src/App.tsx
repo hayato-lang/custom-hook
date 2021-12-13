b6af89a5b340ba254da2ce62aa0f790eee0dce61
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-14 08:59:56 +0900

    カスタムフックを利用しないデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 import { UserCard } from './components/UserCard';
+import { User } from './types/api/user';
+import { UserProfile } from './types/UserProfile';
 
-const user = {
-  id: 1,
-  name: "はやと",
-  email: "aggerenrique@yahoo.co.jp",
-  address: "アース"
-};
 
 function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchData = () => {
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) =>{
+      const data = res.data.map((user) => ({
+        id: user.id,
+        name: `${user.name}(${user.username})`,
+        email: user.email,
+        address: `${user.address.city}${user.address.suite}${user.address.street}`
+      }))
+      setUserProfiles(data);
+    })
+  }
   return (
     <div className="App">
-      <UserCard user={user}/>
+      <button onClick={onClickFetchData}>データ取得ハゲタコ</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user}/>
+      ))}
     </div>
   );
 }
