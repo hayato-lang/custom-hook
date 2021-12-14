2123ff2c63058dfb77b2e5981715bbcfadced89e
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-14 13:47:04 +0900

    カスタムフックの呼び出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import axios from 'axios';
-import { useState } from 'react';
 import './App.css';
 import { UserCard } from './components/UserCard';
-import { User } from './types/api/user';
-import { UserProfile } from './types/UserProfile';
+import { useAllUsers } from './hooks/useAllUsers';
 
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const onClickFetchData = () => {
-    setLoading(true);
-    setError(false);
-
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) =>{
-      const data = res.data.map((user) => ({
-        id: user.id,
-        name: `${user.name}(${user.username})`,
-        email: user.email,
-        address: `${user.address.city}${user.address.suite}${user.address.street}`
-      }))
-      setUserProfiles(data);
-    }).catch(() => {
-      setError(true)
-    }).finally(() => {
-      setLoading(false);
-    });
-  }
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchData = () => getUsers();
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得ハゲタコ</button>
