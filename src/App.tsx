91cfdce6806c8fda66511017c06cdcbdbc14e426
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-14 09:41:57 +0900

    カスタムフック利用せず

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,12 @@ import { UserProfile } from './types/UserProfile';
 
 function App() {
   const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const onClickFetchData = () => {
+    setLoading(true);
+    setError(false);
+
     axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) =>{
       const data = res.data.map((user) => ({
         id: user.id,
@@ -17,14 +22,27 @@ function App() {
         address: `${user.address.city}${user.address.suite}${user.address.street}`
       }))
       setUserProfiles(data);
-    })
+    }).catch(() => {
+      setError(true)
+    }).finally(() => {
+      setLoading(false);
+    });
   }
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得ハゲタコ</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user}/>
-      ))}
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ): loading? (
+        <p>Loading...</p>
+      ) : (
+        <>
+        {userProfiles.map((user) => (
+          <UserCard key={user.id} user={user}/>
+        ))}
+        </>
+      )}
     </div>
   );
 }
